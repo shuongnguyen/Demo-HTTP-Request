737cb43010790af0a87ee1779f8fb3b0e27f9fe8
shuongnguyen <shuongnguyen.work@gmail.com>
2023-08-25 17:18:24 +0700

    add log_message

--- app.py
+++ app.py
@@ -4,28 +4,29 @@ import json
 from data import User, users
 
 class MainHandler(tornado.web.RequestHandler):
+    def log_message(self, message, *args):
+        print(message % args)
+        
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
-
-    def options(self):
-        self.set_status(204)
-        self.finish()
+        self.set_header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
 
 class UsersHandler(MainHandler):
     def get(self):
+        self.log_message("GET request received")
         user_data = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
         self.write(json.dumps(user_data))
 
     def post(self):
+        self.log_message("POST request received")
         data = json.loads(self.request.body)
         new_id = max([user.id for user in users]) + 1
         new_user = User(new_id, data['username'], data['email'])
         users.append(new_user)
         self.write(json.dumps({'message': 'User added successfully'}))
-
 class UserHandler(MainHandler):
     def get(self, user_id):
+        self.log_message("GET request received")
         user = next((user for user in users if user.id == int(user_id)), None)
         if user:
             self.write(json.dumps({'id': user.id, 'username': user.username, 'email': user.email}))
@@ -40,12 +41,15 @@ class UserHandler(MainHandler):
             user.username = data['username']
             user.email = data['email']
             self.write(json.dumps({'message': 'User updated successfully'}))
+            self.log_message("PUT request received")
         else:
             self.set_status(404)
             self.write(json.dumps({'message': 'User not found'}))
 
+
     def delete(self, user_id):
         global users
+        self.log_message("DELETE request received")
         users = [user for user in users if user.id != int(user_id)]
         self.write(json.dumps({'message': 'User deleted successfully'}))
 
